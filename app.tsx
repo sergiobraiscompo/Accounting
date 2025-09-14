5a62385c47e35d04c2753859b40ea18f690e9f59
sergiobraiscompo <sergio>
2025-09-14 22:02:02 +0200

    first push

--- app.tsx
+++ app.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { Router } from "@/core/router";
-import { ProfileProvider } from "@/core/profile";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./main-style";
 
 export const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <ProfileProvider>
-        <Router />
-      </ProfileProvider>
+
     </ThemeProvider>
   );
 };
